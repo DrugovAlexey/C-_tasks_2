1055a8771ae1fca86d06b49e8b40ef9a81afaa27
Olga <panda146@yandex.ru>
2023-06-15 19:04:02 +0300

    adeedntask 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine(" Введите число");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+
+if (a1 < 100)
+{
+     Console.Write($"Третьей цифры нет");
+    
+}
+
+if (a1 >= 100 & a1 < 1000)
+{
+     int x = a1 % 10;
+           Console.Write($"Искомое число {x}");
+}
+if (a1 > 999)
+    { while (a1 > 999)
+        {
+           a1 /= 10;
+        }
+        int n = a1 % 10;
+        Console.Write($"Искомое число {n}");
+    }
+
