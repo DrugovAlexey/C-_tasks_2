f0d3ca6571cb3830a24f0642d8f916e168451ed6
Olga <panda146@yandex.ru>
2023-06-15 19:02:47 +0300

    adeedntask 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число - ");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+
+int b = a1 / 10;
+int c = b % 10;
+
+Console.Write($"Искомое число {c}");
