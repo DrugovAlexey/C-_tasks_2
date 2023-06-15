8e3afd0c13ddf44b627a6568e127d411be335cdb
Olga <panda146@yandex.ru>
2023-06-15 19:05:19 +0300

    adeedntask 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+string a = Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+
+if (a1 > 7 || a1 < 1)
+{
+    
+    Console.Write($"Неверно введено значение");
+}
+
+if (a1 == 6 || a1 == 7)
+{
+    
+    Console.Write($"Да");
+}
+if (a1 < 6 & a1 > 0)
+{
+    
+    Console.Write($"Нет");
+}
